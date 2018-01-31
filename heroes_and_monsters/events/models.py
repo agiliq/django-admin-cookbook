0bcf7ca73128a85dd82a1a1d41595500443104fb
Shabda Raaj <shabda@agiliq.com>
2018-01-31 16:46:03 +0530

    Added gitignore, and event models

--- heroes_and_monsters/events/models.py
+++ heroes_and_monsters/events/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 from entities.models import Hero, Villain
 
-class Event(models.Model):
+class Epic(models.Model):
     name = models.CharField(max_length=255)
     participating_heroes = models.ManyToManyField(Hero)
-    participating_villains = models.ManyTomanyField(Villain)
+    participating_villains = models.ManyToManyField(Villain)
+
+
+class Event(models.Model):
+    epic = models.ForeignKey(Epic, on_delete=models.CASCADE)
+    details = models.TextField()
+    years_ago = models.PositiveIntegerField()
+
+
+class EventHero(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    hero = models.ForeignKey(Hero, on_delete=models.CASCADE)
+    is_primary = models.BooleanField()
+
+
+class EventVillain(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    hero = models.ForeignKey(Villain, on_delete=models.CASCADE)
+    is_primary = models.BooleanField()
