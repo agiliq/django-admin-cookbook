be88379546a141445cb028fafb0e3444cbb23068
Shabda Raaj <shabda@agiliq.com>
2018-02-01 12:01:28 +0530

    testing images

--- docs/change_text.rst
+++ docs/change_text.rst
@@ -1,2 +1,7 @@
 How to change 'Django administration' text?
 ================================================
+
+
+.. image:: images/default_login.png
+.. image:: images/default_listview.png
+.. image:: images/default_title.png
--- heroes_and_monsters/events/admin.py
+++ heroes_and_monsters/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Epic, Event, EventHero, EventVillain
+
+
+admin.site.register(Epic)
+admin.site.register(Event)
+admin.site.register(EventHero)
+admin.site.register(EventVillain)
